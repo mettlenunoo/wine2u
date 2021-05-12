b2a5da103fcbd220d27197782653fc02f1c837b7
mettlenunoo <40646025+mettlenunoo@users.noreply.github.com>
2021-05-12 09:43:23 +0000

    single-product image fix
    single-product image fix

--- resources/views/pages/single_product.blade.php
+++ resources/views/pages/single_product.blade.php
@@ -487,13 +487,13 @@
                         <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
                             <div class="carousel-inner">
                                 <div class="carousel-item active">
-                                    <img src="https://via.placeholder.com/500" class="d-block w-100" alt="...">
+                                    <img src="https://via.placeholder.com/500" class="d-block product-image" alt="...">
                                 </div>
                                 <div class="carousel-item">
-                                    <img src="https://via.placeholder.com/500" class="d-block w-100" alt="...">
+                                    <img src="https://via.placeholder.com/500" class="d-block product-image " alt="...">
                                 </div>
                                 <div class="carousel-item">
-                                    <img src="https://via.placeholder.com/500" class="d-block w-100" alt="...">
+                                    <img src="https://via.placeholder.com/500" class="d-block product-image " alt="...">
                                 </div>
                             </div>
                             <button class="carousel-control-prev" type="button"
