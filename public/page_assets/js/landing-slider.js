61c7ec5adb4975998e2a72f6f2582fabbc631734
Falola Oluwatobi <tobi@effectstudios.co>
2021-05-14 01:06:40 +0800

    Food pairing slider

--- public/page_assets/js/landing-slider.js
+++ public/page_assets/js/landing-slider.js
@@ -1,12 +1,43 @@
-document.addEventListener("DOMContentLoaded", function() {
-	$('.landing-slider').slick({
+document.addEventListener("DOMContentLoaded", function () {
+    $('.landing-slider').slick({
         dots: false,
         arrows: false,
         infinite: true,
         speed: 300,
         slidesToShow: 1,
         slidesToScroll: 1,
-		autoplay: true,
-		fade: true
+        autoplay: true,
+        fade: true
     });
+
+    $('.fp-slides').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        responsive: [{
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3
+                }
+			},
+			{
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ]
+	});
+	
+	$(".fp-slider #fp-prev").click(function() {
+		$(".fp-slides").slick("slickPrev");
+	});
+
+	$(".fp-slider #fp-next").click(function() {
+		$(".fp-slides").slick("slickNext");
+	});
 })
