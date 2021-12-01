adc68b33ab48c0e5df83cb7a47e3051098761bd2
DawudAbdul12 <dabdulmanan@gmail.com>
2021-12-01 03:11:07 +0100

    Update header.blade.php

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 {{-- @if (trim($slot) === 'Laravel') --}}
-<img src="/page_assets/img/favicon.png" class="logo" alt="wine2u">
+<img src="https://wine2u.com/page_assets/img/favicon.png" class="logo" alt="wine2u">
 {{-- @else --}}
 {{-- {{ $slot }} --}}
 {{-- @endif --}}
