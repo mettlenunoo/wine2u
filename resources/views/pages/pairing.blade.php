dc192452fe2cd6d25ecbc5ecfad0422133f4f452
DawudAbdul12 <dabdulmanan@gmail.com>
2021-12-02 14:57:08 +0100

    Update pairing.blade.php

--- resources/views/pages/pairing.blade.php
+++ resources/views/pages/pairing.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Wine2u | Blog</title>
+    <title>Wine2u | Pairing</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
@@ -44,14 +44,11 @@
             
                 <div class="col-12 col-md-4 px-md-4 mb-4">
                     <a href="/blog/{{$pairing->blog_id}} ">
-                        <img src="/blog_images/{{ $pairing->image }}" class="bolg_img mb-2" alt="{{ ucwords($pairing->title)}}">
+                        <img src="/images/{{ $pairing->image }}" class="bolg_img mb-2" alt="{{ ucwords($pairing->title)}}">
                         <p class="f_titile_p2 mb-0 py-3"> {{ ucwords($pairing->title)}} </p>
                         {{-- <p class="f_titile_p3"> {{ strwords($blog->content, 150) }} </p> --}}
                     </a>
-                    {{-- <div class="">
-                        <span class="pr-2"><img src="/page_assets/img/wine2ulogo.svg" class="userside_blog" alt=""></span>
-                        <span class="pr-2"> {{ ucwords($blog->author)}} </span>
-                    </div> --}}
+               
                 </div>
 
             @endforeach
