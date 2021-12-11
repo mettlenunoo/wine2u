8f65f0aecf76fb165fe4e696236555365d393bcc
DawudAbdul12 <dabdulmanan@gmail.com>
2021-12-11 09:03:01 +0100

    Update thankyou.blade.php

--- resources/views/pages/thankyou.blade.php
+++ resources/views/pages/thankyou.blade.php
@@ -24,13 +24,11 @@
   <div class="container  ">
     <div class="row">
        <div class="col-12 col-md-7 mx-auto text-center ">
-        
             <h2>HI, {{ session()->get('paymentinfo')['fullname'] }}</h2>
             <img src="{{ asset('images/summary.png')}}" class="img-responsive">
-            <p>Your order has now been confirmed.</p>
-            <p>We will send you some inbox love when your order leaves our warehouse.</p>
-            <a href="\index"><button class="btn btn-wine2u">CONTINUE SHOPPING <span class="pl-2 text-right"><img src="{{ asset('img/icons/arrorright.svg')}}" width="8" alt=""></span></button></a>
-        
+            <p>Thank you for the purchase.</p>
+            <p>We will send you an email confirmation shortly</p>
+            <a href="\index"><button>CONTINUE SHOPPING <span class="pl-2 text-right"><img src="{{ asset('img/icons/arrorright.svg')}}" width="8" alt=""></span></button></a>
        </div>
     </div>
   </div>
