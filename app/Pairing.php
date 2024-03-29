1c481ca04bc5b6da2ccc88d5b3c62eb554d8108f
mettlenunoo <desmondmettlenunoo@gmail.com>
2024-03-29 13:32:30 +0000

    Update Pairing.php

--- app/Pairing.php
+++ app/Pairing.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pairing extends Model
 {
+    protected $fillable = [
+        'parent',
+
+    ];
+
     public function subPairing()
     {
         return $this->hasMany(Pairing::class, 'parent', 'id');
