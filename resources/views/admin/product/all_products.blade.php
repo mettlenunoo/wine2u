7d58885e347242ed8c914e39363a94386020ca81
mettlenunoo <desmondmettlenunoo@gmail.com>
2024-03-30 10:41:25 +0000

    Update all_products.blade.php

--- resources/views/admin/product/all_products.blade.php
+++ resources/views/admin/product/all_products.blade.php
@@ -20,22 +20,22 @@
                 </div>
                 <!-- END Page Header -->
                 @include('admin.message')
-                
+
 
                 <!-- Page Content -->
                 <div class="content visibility-hidden" data-toggle="appear" data-timeout="100" data-class="animated fadeInLeft">
-                   
+
                      <!-- Table Sections (.js-table-sections class is initialized in App() -> uiHelperTableToolsSections()) -->
                     <div class="block">
                         <div class="block-header">
                             {{-- <div class="block-options">
                                 <code>.js-table-sections</code>
                             </div> --}}
-                            {{-- <h3 class="block-title">Table Sections</h3> --}}
+                            {{-- <h3 class="block-title">Table Sections</h3> js-table-sections--}}
                         </div>
                         <div class="block-content">
-                           
-                            <table class="js-table-sections table table-hover">
+
+                            <table class=" table table-hover js-dataTable-full">
                                 <thead>
                                     <tr>
                                         <th style="width: 30px;"></th>
@@ -49,9 +49,9 @@
                                         {{-- <th class="hidden-xs" style="width: 15%;">Date</th> --}}
                                     </tr>
                                 </thead>
-                            
+
                                 @forelse ($products as $key => $product)
-                                    
+
                                     <tbody class="js-table-sections-header @if($key == 0) open @endif">
                                         <tr>
                                             <td class="text-center">
@@ -63,15 +63,15 @@
                                                 <em class="text-muted"> {{ ucwords($product->product_name)  }}</em>
                                                 {{-- <span class="label label-danger">Disabled</span> --}}
                                             </td>
-                                        
+
                                             <td class="font-w600">
 
                                                 @forelse ($product->categories as $key => $category)
-                                                    <em class="text-muted"> @if($key >= 1) , @endif {{ ucwords($category->title) }} </em>  
+                                                    <em class="text-muted"> @if($key >= 1) , @endif {{ ucwords($category->title) }} </em>
                                                 @empty
                                                     <em class="text-muted">Uncategorized</em>
                                                 @endforelse
-                                                
+
                                             </td>
                                             <td class="">
                                                 <em class="text-muted"> {{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s',  $product->created_at)->format('F j, Y') }}</em>
@@ -80,10 +80,10 @@
                                                 <div class="btn-group">
                                                     <a  href="/admin/product/{{ $product->id }}/edit" class="btn btn-xs btn-default" type="button" data-toggle="tooltip" title="Edit" ><i class="fa fa-pencil"></i></a>
                                                     <a  href="/admin/product/{{ $product->id }}/edit" class="btn btn-xs btn-default" type="button" data-toggle="tooltip" title="View" ><i class="fa fa-eye"></i></a>
-                                                    
+
                                                     {!! Form::open(['action' => ['productController@destroy', $product->id ], 'method' => 'POST']) !!}
                                                     {{ method_field('DELETE') }}
-                                                    <button type="submit" onClick ="return confirm('Are you sure You want to Delete')" class="btn btn-xs btn-default" type="button" data-toggle="tooltip" title="Remove"><i class="fa fa-times"></i></button> 
+                                                    <button type="submit" onClick ="return confirm('Are you sure You want to Delete')" class="btn btn-xs btn-default" type="button" data-toggle="tooltip" title="Remove"><i class="fa fa-times"></i></button>
                                                     {!! Form::close() !!}
                                                     {{-- <a  href="javascript:void(0)" class="btn btn-xs btn-default" type="button" data-toggle="tooltip" title="Remove" onClick ="return deleteAlert('/admin/vendor/delete?del=')"><i class="fa fa-times"></i>
                                                     </a> --}}
@@ -99,12 +99,12 @@
                                         <td class="text-center"></td>
                                         <td class="font-w600 text-success">
 
-                                          @if($vProduct->sale_price > 0) 
+                                          @if($vProduct->sale_price > 0)
 
                                             <span class="text-success"> {{  number_format($vProduct->sale_price,2)  }}</span> <br/>
                                             <span class="text-danger" style=" text-decoration: line-through "> {{ number_format( $vProduct->regular_price,2 ) }}</span>
-                                          
-                                           @else 
+
+                                           @else
 
                                             <span class="text-success"> {{  number_format($vProduct->regular_price,2)  }}</span>
 
@@ -113,7 +113,7 @@
                                         </td>
 
                                         <td class="font-w600">
-                                            <small> <b> {{ $vProduct->attribute->parentAttribute->title }} </b> {{ $vProduct->attribute->title }} </small> 
+                                            <small> <b> {{ $vProduct->attribute->parentAttribute->title }} </b> {{ $vProduct->attribute->title }} </small>
                                         </td>
 
                                         <td class="font-w600">
@@ -122,7 +122,7 @@
                                             @else
                                                 <small class="text-muted text-danger"> {{ $vProduct->stock }} </small>
                                             @endif
-                                            
+
                                         </td>
 
                                         <td class="font-w600">
@@ -138,18 +138,18 @@
                                      @empty
                                          <tr>
                                             <td class="text-center"></td>
-                                           <td style=""> No variable product </td> 
+                                           <td style=""> No variable product </td>
                                          </tr>
                                      @endforelse
-                                      
+
                                     </tbody>
 
                                 @empty
                                 <tr>
                                   <td class="text-center" style=""></td>
-                                    <td style=""> <p> No product. <a href="/product/create"> Please add a new product</a> </p> </td> 
+                                    <td style=""> <p> No product. <a href="/product/create"> Please add a new product</a> </p> </td>
                                  </tr>
-                                    
+
 
                                 @endforelse
 
